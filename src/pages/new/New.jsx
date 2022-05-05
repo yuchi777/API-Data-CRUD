f7be3ef4ab7c1acdf15af1861636e4bfc7e0f8a3
yuchi <yuchi8943@gmail.com>
2022-05-05 15:49:18 +0800

    edit new page use props

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,14 +2,14 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
-const New = () => {
+const New = ({inputs,title}) => {
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1>Add New User</h1>
+          <h1>{title}</h1>
         </div>
         <div className="bottom">
           <div className="left">
@@ -23,34 +23,16 @@ const New = () => {
                 </label>
                 <input type="file" id="file" style={{display:"none"}}/>
               </div>
-              <div className="formInput">
-                <label>Username</label>
-                <input type="text" placeholder="john_doe" />
+
+              {inputs.map((input) => (
+
+              <div className="formInput" key={input.id}>
+                <label>{input.label}</label>
+                <input type={input.type} placeholder={input.placeholder} />
               </div>
-              <div className="formInput">
-                <label>Name and surname</label>
-                <input type="text" placeholder="John Doe" />
-              </div>
-              <div className="formInput">
-                <label>Email</label>
-                <input type="email" placeholder="john_doe@gmail.com" />
-              </div>
-              <div className="formInput">
-                <label>Phone</label>
-                <input type="text" placeholder="123456789" />
-              </div>
-              <div className="formInput">
-                <label>Password</label>
-                <input type="password"  />
-              </div>
-              <div className="formInput">
-                <label>Address</label>
-                <input type="password" placeholder="123 St. 234 NewYork" />
-              </div>
-              <div className="formInput">
-                <label>Country</label>
-                <input type="password" placeholder="USA" />
-              </div>
+              
+              ))}
+
               <button>Send</button>
             </form>
           </div>
