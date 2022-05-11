69fe366a016aeeba9d0f12b661d284b0a605e987
yuchi <yuchi8943@gmail.com>
2022-05-11 17:48:49 +0800

    add material table

--- src/components/materialTable/materialTable.jsx
+++ src/components/materialTable/materialTable.jsx
@@ -1,34 +1,49 @@
-// import React from 'react'
-// import MaterialTable from 'material-table';
-// const materialTable = () => {
-//   return (
-//     <div>
-//         <h3>
-//             materialTable
-//         </h3>
-//         <MaterialTable
-        
-//       title="Basic Filtering Preview"
-//       columns={[
-//         { title: 'Name', field: 'name' },
-//         { title: 'Surname', field: 'surname' },
-//         { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-//         {
-//           title: 'Birth Place',
-//           field: 'birthCity',
-//           lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-//         },
-//       ]}
-//       data={[
-//         { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-//         { name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34 },
-//       ]}        
-//       options={{
-//         filtering: true
-//       }}
-//     />
-//     </div>
-//   )
-// }
+import * as React from 'react';
+import "./materialTable.scss";
+import { DataGrid } from '@mui/x-data-grid';
 
-// export default materialTable
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 20 },
+  { field: 'status', headerName: '連線狀況', width: 100 },
+  { field: 'group', headerName: '群組', width: 130 },
+  { field: 'hostname', headerName: '主機名稱', width: 130 },
+  { field: 'owner', headerName: '保管者', width: 130 },
+  { field: 'cpu', headerName: 'cpu', width: 20 },
+  { field: 'mem', headerName: 'mem', width: 20 },
+  { field: 'swap', headerName: 'swap', width: 20 },
+  { field: 'lasttime', headerName: '最後時間', width: 100 },
+  { field: 'alertmsg', headerName: '告警訊息', width: 130 },
+  { field: 'info', headerName: '資訊', width: 130 }
+];
+
+const rows   = [
+  { id:1,status: 'on', group: 'Snow', hostname: 'Jon', owner: 35,cpu:5, mem:'16g',swap:523,lasttime:1200,alertmsg:'良好',info:'無訊息' },
+  { id:2,status: 'on', group: 'Lannister', hostname: 'Cersei', owner: 42,cpu:52, mem:'162g',swap:5232,lasttime:12002,alertmsg:'良好2',info:'無訊息2' },
+  { id:4,status: 'of', group: 'Lannister', hostname: 'Jaime', owner: 45,cpu:5, mem:'16g',swap:523,lasttime:1200,alertmsg:'良好',info:'無訊息' },
+  { id:5,status: 'of', group: 'Stark', hostname: 'Arya', owner: 16,cpu:5, mem:'16g2',swap:5232,lasttime:12002,alertmsg:'良好2',info:'無訊息2' },
+  { id:6,status: 'on', group: 'Targaryen', hostname: 'Daenerys', owner:23,cpu:5, mem:'16g',swap:523,lasttime:1200,alertmsg:'良好',info:'無訊息' },
+  { id:7,status: 'of', group: 'Melisandre', hostname: null, owner: 15,cpu:56, mem:'166g',swap:5236,lasttime:12006,alertmsg:'良好6',info:'無訊息6' },
+  { id:8,status: 'on', group: 'Clifford', hostname: 'Ferrara', owner: 44,cpu:5, mem:'16g',swap:523,lasttime:1200,alertmsg:'良好',info:'無訊息' },
+  { id:9,status: 'of', group: 'Frances', hostname: 'Rossini', owner: 36,cpu:65, mem:'166g',swap:5236,lasttime:12006,alertmsg:'良好6',info:'無訊息6' },
+  { id:10,status: 'on', group: 'Roxie', hostname: 'Harvey', owner: 65,cpu:5, mem:'16g',swap:523,lasttime:1200,alertmsg:'良好',info:'無訊息' },
+];
+
+export default function MaterialTable() {
+
+
+
+  return (
+
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        />
+      </div>
+    
+  );
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,25 +3,30 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Widget from "../../components/widget/Widget";
 import Chart from "../../components/chart/Chart";
-import Table from "../../components/table/Table";
+// import Table from "../../components/table/Table";
+import MaterialTable from "../../components/materialTable/MaterialTable.jsx";
 
 const Home = () => {
   return (
     <div className="home"> 
       <Sidebar/>
-      <div className="homeContainer">
+      <div className="homeContainer"> 
         <Navbar/>
         <div className="widgets">
           <Widget type="all"/>
           <Widget type="normal"/>
           <Widget type="warning"/>
         </div>
-        <div className="charts">
-          <Chart title="Last 6 Months (Revenue)" aspect={3/1}/>
+        <div className="listContainer">
+          <div className="listTitle">告警列表</div>
+          <MaterialTable />
         </div>
         <div className="listContainer">
-          <div className="listTitle">Latest Transactions</div>
-          <Table />
+          <div className="listTitle">機台一欄</div>
+          <MaterialTable />
+        </div>
+        <div className="charts">
+          <Chart title="Last 6 Months (Revenue)" aspect={3/1}/>
         </div>
       </div>
     </div>
